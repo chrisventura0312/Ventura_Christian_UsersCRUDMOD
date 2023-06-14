9bbb21f39c2c3a65b92db592b0d20f46299549a8
Chris Ventura <cvent820@gmail.com>
2023-06-14 13:50:25 -0700

    initial commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,48 @@
+from flask_app import app, render_template, redirect, request
+from flask_app.models.user import User
+@app.route('/')
+def index():
+    return redirect('/users')
+# Render the users page
+@app.route('/users')
+def view_users():
+    return render_template("Read(All).html",users=User.get_all())
+# Render the new user page
+@app.route('/users/new')
+def new_user():
+    return render_template('Create.html')
+# Create a new user
+@app.route('/users/create', methods=['POST'])
+def create_user():
+    if not User.validate_user(request.form):
+        return redirect('/users/new')
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+# Update a user
+@app.route('/users/<int:id>/edit')
+def edit_user(id):
+    return render_template('Update.html', user=User.get_one(id))
+# Update a user
+@app.route('/users/<int:id>/update', methods=['POST'])
+def update_user(id):
+    form_data = {
+        'id': id,
+        'first_name': request.form.get('first_name'),
+        'last_name': request.form.get('last_name'),
+        'email': request.form.get('email'),
+    }
+    print("Form Data:", form_data)  # Print the form data for debugging purposes
+    result = User.update(form_data)
+    print("Update Result:", result)  # Print the update result for debugging purposes
+    return redirect('/users')
+
+# Delete a user
+@app.route('/users/<int:id>/delete')
+def delete_user(id):
+    User.delete(id)
+    return redirect('/users')
+# View a user+
+@app.route('/users/<int:id>')
+def view_user(id):
+    return render_template('Read(One).html', user=User.get_one(id))
